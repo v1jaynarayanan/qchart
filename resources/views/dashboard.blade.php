3a63b59409f2ae7fa23c7c73b96702b2a15ac719
Vijay Narayanan <vijaynarayanan@Vijays-MacBook-Pro.local>
2016-07-03 22:58:25 +0530

    fail auth if user not in confirmed status

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,27 +3,35 @@
 @section('content')
 <div class="container sub-page">
     <div class="clearfix">
-        <h1 class="fL">Dashboard</h1>
+        <h1 class="fL">Survey Dashboard</h1>
+        @if (Auth::user()->role_id == 0)
         <a href="#" class="btn survey-btn fR">Add New Survey</a>
+        @endif
     </div>
     <table class="table">
         <tr>
             <th width="30">S.No.</th>
             <th>Survey Title</th>
+            <th>Created By</th>
             <th>Survey Status</th>
             <th>Created Date Time</th>
             <th>Updated Date Time</th>
-            <th width="90">&nbsp;</th>
+            @if (Auth::user()->role_id == 0)
+                <th width="90">&nbsp;</th>
+            @endif
         </tr>
         @if (isset($surveys) && !empty($surveys))
             @foreach ($surveys as $i => $value) 
             <tr>
                 <td>{{ ++$i }} </td>
                 <td><a href="#">{{ $value->title }}</a></td>
+                <td>{{ $value->name }}</td>
                 <td>{{ $value->status == 1 ? 'Open' : 'Closed' }}</td>
                 <td>{{ $value->created_at }}</td>
                 <td>{{ $value->updated_at }}</td>
-                <td><a href="#" class="btn delete-btn">Delete</a></td>
+                @if (Auth::user()->role_id == 0)
+                    <td><a href="#" class="btn delete-btn">Delete</a></td>
+                @endif
             </tr>
             @endforeach
         @endif    
