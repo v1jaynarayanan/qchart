269017cb3b3caf76dc593668ed0620bb74aa2dc8
Vijay Narayanan <vijaynarayanan@Vijays-MacBook-Pro.local>
2016-07-03 23:21:43 +0530

    Stylesheet changes

--- resources/views/auth/registration_confirmation.blade.php
+++ resources/views/auth/registration_confirmation.blade.php
@@ -1,18 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Registration confirmation</div>
-
-                <div class="panel-body">
-                    You have successfully registered. An email has been sent to your email id for verification. <br>
-                    Check your email and click on the link provided in the email to activate your account.
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="container sub-page">
+    <h1>Registration confirmation</h1>
+        <p>You have been successfully registered. An email has been sent to your email id for verification. <br>
+            Check your email and click on the link provided in the email to activate your account.</p>
 </div>
 @endsection
