24bc630fc981fdfdfa9305c1a6f13070d8d05699
Vijay Narayanan <vijaynarayanan@Vijays-MacBook-Pro.local>
2016-07-04 18:56:09 +0530

    Implement simple pagination

--- database/seeds/surveyTableSeeder.php
+++ database/seeds/surveyTableSeeder.php
@@ -35,5 +35,53 @@ class SurveyTableSeeder extends Seeder
             'description' => 'Survey to get feedback on what went well and what did not for Sprint 1',
             'status' => 0,
         ]);
+
+        \App\Survey::create([
+            'user_id' => '1',
+            'title' => 'Sprint2 Retrospective',
+            'slug' => 'Sp2 Retro',
+            'description' => 'Survey to get feedback on what went well and what did not for Sprint 2',
+            'status' => 1,
+        ]);
+
+        \App\Survey::create([
+            'user_id' => '1',
+            'title' => 'Sprint3 Retrospective',
+            'slug' => 'Sp3 Retro',
+            'description' => 'Survey to get feedback on what went well and what did not for Sprint 3',
+            'status' => 1,
+        ]);
+
+        \App\Survey::create([
+            'user_id' => '1',
+            'title' => 'Sprint4 Retrospective',
+            'slug' => 'Sp4 Retro',
+            'description' => 'Survey to get feedback on what went well and what did not for Sprint 4',
+            'status' => 1,
+        ]);
+
+        \App\Survey::create([
+            'user_id' => '1',
+            'title' => 'Sprint5 Retrospective',
+            'slug' => 'Sp5 Retro',
+            'description' => 'Survey to get feedback on what went well and what did not for Sprint 5',
+            'status' => 1,
+        ]);
+
+        \App\Survey::create([
+            'user_id' => '1',
+            'title' => 'Sprint6 Retrospective',
+            'slug' => 'Sp6 Retro',
+            'description' => 'Survey to get feedback on what went well and what did not for Sprint 6',
+            'status' => 1,
+        ]);
+
+        \App\Survey::create([
+            'user_id' => '1',
+            'title' => 'Sprint7 Retrospective',
+            'slug' => 'Sp7 Retro',
+            'description' => 'Survey to get feedback on what went well and what did not for Sprint 7',
+            'status' => 1,
+        ]);
     }
 }
