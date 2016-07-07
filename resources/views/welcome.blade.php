c7001391b2cfb1c2097e58e3d1a6ddaa65a39ad2
Vijay Narayanan <vijaynarayanan@Vijays-MacBook-Pro.local>
2016-07-07 14:33:07 +0530

    Fix background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container sub-page">
-    <h1>Welcome to QChart</h2>
-        <h3>QChart is a simple tool that helps you to</h3> 
-        <ul class="common-list"> 
-            <li> Create a survey questionnaire online </li> 
-            <li> Send the questionnaire to recipients for completion and </li>
-            <li> Generate and display a spider graph online </li>
-        </ul>
-</div>
+<div class="bg-frame">
+		<div class="table-row">
+			<div class="sub-page">
+				<h1>Welcome to QChart</h2>
+		        <h3>QChart is a free, simple and easy tool that helps you to</h3> 
+		        <ul class="common-list"> 
+		            <li> Create a survey questionnaire online </li> 
+		            <li> Send the questionnaire to recipients to get answers </li>
+		            <li> Generate and display a spider graph  </li>
+		        </ul>
+			</div>
+		</div>
+	</div>
 @endsection
