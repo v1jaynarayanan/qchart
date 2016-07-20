3a3f1438b2ff553f4a9a99dfe2f86177c0fc4e45
Vijay Narayanan <vijaynarayanan@Vijays-MacBook-Pro.local>
2016-07-20 22:44:11 +0530

    Dashboard clean up

--- public/js/spider.js
+++ public/js/spider.js
@@ -35,21 +35,29 @@ function validateForm() {
 $(function(){
     // Login validation
     $('#loginForm input').on('keyup', function(){
-        var empty = false;
+        var empty = false, isValid = true;
         var x = document.forms["myForm"]["email"].value;
         var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
         $('#loginForm input').each(function() {
             if ($(this).val() == '' || reg.test(x) == false) {
                 empty = true;
             }
+            if ( $(this).val() === '' ) {
+                isValid = false;
+            }
         });
+        // Enable button all input values is correct
         if (empty) {
             $('#submitBtn').attr('disabled', 'disabled');
         } else {
             $('#submitBtn').removeAttr('disabled');
         }
-        validateForm();
+        // Validate form if all fields value entered
+        if(isValid) {
+            validateForm();
+        }
     });
+    // Code for safari label jumbing issue
     $('input[name="email"]').on('keypress', function(){
         if($(this).val() !== "") {
             $(this).addClass('invalid-field');
@@ -58,6 +66,72 @@ $(function(){
         }
     });
     viewportH();
+    // Add new survey
+    var count = 1, $addNewSurveyBtn = $('#addNewSurvey'), $addNewQuestion = $('#addNewQuestion'), $addNewQuestionDiv = $('#addNewQuestionDiv');
+    $addNewQuestion.on('click', function(){
+        var $this = $(this), appendContent = '<div class="form-row"><label class="qnsLabel">New question '+(count+1)+'</label><input type="text" class="textBox" /></div>';
+        $addNewQuestionDiv.hide();
+        if(count < 8) {
+            $(appendContent).insertBefore($this.parent());
+            $addNewSurveyBtn.attr('disabled', true);
+        } else {
+            $this.hide();
+            return false;
+        }
+        if(count === 8) {
+            $this.hide();
+        }
+        count++;
+    });
+    // New survey form validation
+    $('body').on('keyup', '.formMainRow', function(){
+        var isValid = true;
+        $(this).find('input').each(function(){
+            if ( $(this).val() === '' ) {
+                isValid = false;
+            }
+        });
+        if(isValid) {
+            $addNewSurveyBtn.attr('disabled', false);
+            $addNewQuestionDiv.show();
+        } else {
+            $addNewSurveyBtn.attr('disabled', true);
+            $addNewQuestionDiv.hide();
+        }
+    });
+    // Delete row
+    var $popUpContainer = $('#popUpOverlay'), $popUp = $('#popUp');
+    $('#deleteSelected').on('click', function(){
+        $(this).parents('tr').addClass('current-row');
+        $popUpContainer.fadeIn();
+        $popUp.animate({marginTop:'-71px'}, 400);
+        return false;
+    });
+    $('#confirmBtn').on('click', function(){
+        $popUpContainer.fadeOut();
+        $popUp.animate({marginTop:'-101px'}, 400);
+        document.dashboardForm.submit();
+
+        return false;
+    });
+    $('#cancelBtn').on('click', function(){
+        $popUpContainer.fadeOut();
+        $popUp.animate({marginTop:'-101px'}, 400);
+        return false;
+    });
+    // Toggle check
+    $dashTable = $('#dashboardTable');
+    $('#checkAll').on('click', function(){
+        if($(this).hasClass('checked-btn') === false) {
+            $(this).html('Uncheck all').addClass('checked-btn');
+            $dashTable.find('input[type="checkbox"]').prop('checked', true);
+        } else {
+            $(this).html('Check all').removeClass('checked-btn');
+            $dashTable.find('input[type="checkbox"]').prop('checked', false);
+        }
+        return false;
+    });
+   
 });
 $(window).load(function(){
     viewportH();
@@ -65,17 +139,3 @@ $(window).load(function(){
 $(window).resize(function(){
     viewportH();
 });
-
-function CheckAll()
-{
-   var arrCheckBoxes = document.dashboardForm.getElementsByClassName("checkbox-btn");
-   for (var i = 0; i < arrCheckBoxes.length; i++) {
-      arrCheckBoxes[i].checked = true;
-   }
-}
-
-function UnCheckAll(chk)
-{
-    for (i = 0; i < chk.length; i++)
-        chk[i].checked = false ;
-}
